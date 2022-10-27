9e0b1eb80767ad071df7eba276d6a40c11fad405
Błażej Kowol <kowol.blazej@gmail.com>
2022-10-27 23:53:53 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@
   });*/
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -14,9 +15,12 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.titles a.active');
 
